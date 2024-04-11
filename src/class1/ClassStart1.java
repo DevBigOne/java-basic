89b9d04f8133931bd32c77ed31b8c61c0b3cc4d0
KimDevloper <a62387272@gmail.com>
2024-04-12 00:32:58 +0900

    Initial commit

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,10 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생!";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+    }
+}
